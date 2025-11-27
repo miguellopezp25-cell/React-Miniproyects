5626a2b676c2aee26a5de94f8fcf92962d0013fb
miguel <miguel.lopezp25@gmail.com>
2025-11-26 20:36:23 -0600

    add shacd for get interactive components

--- hooks/vite.config.ts
+++ hooks/vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-  , tailwindcss()
-  ],
+plugins: [tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
